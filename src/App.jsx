ca1a0796644df6f7991417b3f09a0e72b566654b
Jasonzj <925645402@qq.com>
2017-10-28 09:20:31 +0800

    app.css change scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import Router from 'router'     // 路由配置
-import store from 'ducks'   // store
-import './app.css'              // 全局样式
+import store from 'ducks'       // store
+import './app.scss'             // 全局样式
 
 const App = () => (
     <Provider store={store}>
