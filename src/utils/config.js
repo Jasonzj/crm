6524b4b904b5e2cccf1a6dc79e332addda0a2cbe
Jasonzj <925645402@qq.com>
2017-11-12 19:53:28 +0800

    [utils]: config: update footerText

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export default {
     name: 'CRM',
-    footerText: 'CRM Admin © 2017 Jason',
+    footerText: 'CRM Admin © 2017 JasonZJ',
     openPages: ['/sign_in', '/sign_up'],
     noBackgroundPages: ['/user/*', '/contract/*', '/dashboard'],
     homePage: '/dashboard'
