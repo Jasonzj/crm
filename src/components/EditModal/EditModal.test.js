e5bf151c0878e0f1a4a187d7be409e7e0fa23003
Jasonzj <925645402@qq.com>
2017-11-17 16:13:59 +0800

    [EditModal]: add test

---
+++ src/components/EditModal/EditModal.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import EditModal from './EditModal'
+import { createForm } from 'utils/formConfig'
+
+const setup = () => {
+    const props = {
+        item: {},
+        eid: 1233213123,
+        type: 'visit',
+        onOk: jest.fn(),
+        title: '创建/更新拜访',
+        visible: true,
+        onCancel: jest.fn(),
+        formData: createForm('visit'),
+    }
+
+    props.item = {
+        id: '330000201410087258',
+        uid: '330000201410087258',
+        eName: '李四',
+        name: '顶峰有限公司',
+        result: 0,
+        time: '2006-09-01',
+        note: '备注xxxxxxxxxxxxxxxxxxxxx',
+        content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        key: '330000201410087258'
+    }
+
+    const wrapper = mount(<EditModal {...props} />)
+
+    return {
+        props,
+        wrapper
+    }
+}
+
+describe('EditModal component', () => {
+    const { wrapper, wrapper2, props } = setup()
+
+    it('renders EditModal', () => {
+        expect(wrapper.find('.ant-modal').exists())
+    })
+
+    it('props item name', () => {
+        expect(wrapper.props().item.name).toContain(props.item.name)
+    })
+
+})
