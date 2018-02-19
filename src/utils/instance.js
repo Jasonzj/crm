f5a00841c973648e601057b10e8da24a0b4dc5c7
Jasonzj <925645402@qq.com>
2018-02-19 14:11:07 +0800

    [utils]: instance: update timeout

--- src/utils/instance.js
+++ src/utils/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: !__DEV__ ? 'https://www.easy-mock.com/mock/59e6fb7d750b1a6a0b9ad955' : false,
-    timeout: 10000
+    timeout: 11000
 })
 
 export default instance
